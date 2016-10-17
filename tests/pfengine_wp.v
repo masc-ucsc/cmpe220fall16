6b912bcb0aa654c2ea77ce2b5d912c394e77bb62
Akash Sridhar <aksridha@UCSC.EDU>
2016-10-17 15:57:00 -0700

    pfengine wrapper update

--- tests/pfengine_wp.v
+++ tests/pfengine_wp.v
@@ -10,40 +10,57 @@ module pfengine_wp (
   ,input PF_delta_type             pfgtopfe_op_d
   ,input PF_weigth_type            pfgtopfe_op_w
   ,input SC_laddr_type             pfgtopfe_op_laddr
+  ,input SC_sptbr_type             pfgtopfe_op_sptbr
   //,input  I_pfgtopfe_op_type       pfgtopfe_op 
 
   ,output logic                    pftodc_req0_valid
   ,input  logic                    pftodc_req0_retry
-  ,output I_pftocache_req_type     pftodc_req0
+  ,output SC_laddr_type            pftodc_req0_laddr
+  ,output SC_sptbr_type            pftodc_req0_sptbr 
+  //,output I_pftocache_req_type     pftodc_req0
  
   ,output logic                    pftol2_req0_valid
   ,input  logic                    pftol2_req0_retry
-  ,output I_pftocache_req_type     pftol2_req0
+  ,output SC_laddr_type            pftol2_req0_laddr
+  ,output SC_sptbr_type            pftol2_req0_sptbr
+  //,output I_pftocache_req_type     pftol2_req0
 
   ,output logic                    pftodc_req1_valid
   ,input  logic                    pftodc_req1_retry
-  ,output I_pftocache_req_type     pftodc_req1
+  ,output SC_laddr_type            pftodc_req1_laddr
+  ,output SC_sptbr_type            pftodc_req1_sptbr
+  //,output I_pftocache_req_type     pftodc_req1
     
   ,output logic                    pftol2_req1_valid
   ,input  logic                    pftol2_req1_retry
-  ,output I_pftocache_req_type     pftol2_req1
+  ,output SC_laddr_type            pftol2_req1_laddr
+  ,output SC_sptbr_type            pftol2_req1_sptbr
+  //,output I_pftocache_req_type     pftol2_req1
 
 `ifdef SC_4PIPE
   ,output logic                    pftodc_req2_valid
   ,input  logic                    pftodc_req2_retry
-  ,output I_pftocache_req_type     pftodc_req2
+  ,output SC_laddr_type            pftodc_req2_laddr
+  ,output SC_sptbr_type            pftodc_req2_sptbr
+  //,output I_pftocache_req_type     pftodc_req2
 
   ,output logic                    pftodc_req3_valid
   ,input  logic                    pftodc_req3_retry
-  ,output I_pftocache_req_type     pftodc_req3
+  ,output SC_laddr_type            pftodc_req3_laddr
+  ,output SC_sptbr_type            pftodc_req3_sptbr
+  //,output I_pftocache_req_type     pftodc_req3
 
   ,output logic                    pftol2_req2_valid
   ,input  logic                    pftol2_req2_retry
-  ,output I_pftocache_req_type     pftol2_req2
+  ,output SC_laddr_type            pftol2_req2_laddr
+  ,output SC_sptbr_type            pftol2_req2_sptbr
+  //,output I_pftocache_req_type     pftol2_req2
 
   ,output logic                    pftol2_req3_valid
   ,input  logic                    pftol2_req3_retry
-  ,output I_pftocache_req_type     pftol2_req3
+  ,output SC_laddr_type            pftol2_req3_laddr
+  ,output SC_sptbr_type            pftol2_req3_sptbr
+  //,output I_pftocache_req_type     pftol2_req3
 `endif
 
   ,output PF_cache_stats_type      pf_dcstats  
@@ -75,40 +92,49 @@ module pfengine_wp (
   ,.pfgtopfe_op_retry       (pfgtopfe_op_retry)
   ,.pfgtopfe_op             ({pfgtopfe_op_d
                              ,pfgtopfe_op_w
-                             ,pfgtopfe_op_laddr})
+                             ,pfgtopfe_op_laddr
+                             ,pfgtopfe_op_sptbr})
 
-  ,.pftodc_req0_valid   (pftodc_req0_valid)
-  ,.pftodc_req0_retry   (pftodc_req0_retry)
-  ,.pftodc_req0         (pftodc_req0)
+  ,.pftodc_req0_valid       (pftodc_req0_valid)
+  ,.pftodc_req0_retry       (pftodc_req0_retry)
+  ,.pftodc_req0             ({pftodc_req0_laddr
+                             ,pftodc_req0_sptbr})
 
   ,.pftol2_req0_valid   (pftol2_req0_valid)
   ,.pftol2_req0_retry   (pftol2_req0_retry)
-  ,.pftol2_req0         (pftol2_req0)
+  ,.pftol2_req0         ({pftol2_req0_laddr
+                         ,pftol2_req0_sptbr})
 
   ,.pftodc_req1_valid   (pftodc_req1_valid)
   ,.pftodc_req1_retry   (pftodc_req1_retry)
-  ,.pftodc_req1         (pftodc_req1)
+  ,.pftodc_req1         ({pftodc_req1_laddr
+                         ,pftodc_req1_sptbr})
 
   ,.pftol2_req1_valid   (pftol2_req1_valid)
   ,.pftol2_req1_retry   (pftol2_req1_retry)
-  ,.pftol2_req1         (pftol2_req1)
+  ,.pftol2_req1         ({pftol2_req1_laddr
+                         ,pftol2_req1_sptbr})
 
 `ifdef SC_4PIPE
   ,.pftodc_req2_valid   (pftodc_req2_valid)
   ,.pftodc_req2_retry   (pftodc_req2_retry)
-  ,.pftodc_req2         (pftodc_req2)
+  ,.pftodc_req2         ({pftodc_req2_laddr
+                         ,pftodc_req2_sptbr})
 
   ,.pftol2_req2_valid   (pftol2_req2_valid)
   ,.pftol2_req2_retry   (pftol2_req2_retry)
-  ,.pftol2_req2         (pftol2_req2)
+  ,.pftol2_req2         ({pftol2_req2_laddr
+                         ,pftol2_req2_sptbr})
 
   ,.pftodc_req3_valid   (pftodc_req3_valid)
   ,.pftodc_req3_retry   (pftodc_req3_retry)
-  ,.pftodc_req3         (pftodc_req3)
+  ,.pftodc_req3         ({pftodc_req3_laddr
+                         ,pftodc_req3_sptbr})
 
   ,.pftol2_req3_valid   (pftol2_req3_valid)
   ,.pftol2_req3_retry   (pftol2_req3_retry)
-  ,.pftol2_req3         (pftol2_req3)
+  ,.pftol2_req3         ({pftol2_req3_laddr
+                         ,pftol2_req3_sptbr})
 `endif
 
   ,.pf_dcstats          (pf_dcstats)
