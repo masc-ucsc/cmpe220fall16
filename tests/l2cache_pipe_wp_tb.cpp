966e7a2c81b0b59525e7a1d8c306303eed62992f
Yuxiong Zhu <yzhu29@ucsc.edu>
2016-10-17 22:24:04 -0700

    fix l2 errors

---
+++ tests/l2cache_pipe_wp_tb.cpp
@@ -0,0 +1,55 @@
+#include "Vl2cache_pipe_wp.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+#include <list>
+
+#include <time.h>
+
+#define DEBUG_TRACE 1
+
+vluint64_t global_time = 0;
+VerilatedVcdC* tfp = 0;
+
+void advance_half_clock(Vl2cache_pipe_wp *top) {
+#ifdef TRACE
+  tfp->dump(global_time);
+#endif
+
+  top->eval();
+  top->clk = !top->clk;
+  top->eval();
+
+  global_time++;
+  if (Verilated::gotFinish())  
+    exit(0);
+}
+
+void advance_clock(Vl2cache_pipe_wp *top, int nclocks=1) {
+
+  for( int i=0;i<nclocks;i++) {
+    for (int clk=0; clk<2; clk++) {
+      advance_half_clock(top);
+    }
+  }
+}
+
+void sim_finish(bool pass) {
+#ifdef TRACE
+  tfp->close();
+#endif
+
+  if (pass)
+    printf("\nTB:PASS\n");
+  else
+    printf("\nTB:FAILED\n");
+
+  exit(0);
+}
+
+// Define stimulus struct
+//
+
+double sc_time_stamp() {
+  return 0;
+}
