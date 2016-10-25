d9d66ca0c257a3e23d4424a1a096d7d50cfda352
Akash Sridhar <aksridha@UCSC.EDU>
2016-10-25 14:53:36 -0700

    pfengie wrapper update

--- tests/pfengine_wp.v
+++ tests/pfengine_wp.v
@@ -42,21 +42,108 @@ module pfengine_wp (
   ,logic                           pftodc_req3_l2
 `endif
 
-  ,output PF_cache_stats_type      pf_dcstats  
-  ,output PF_cache_stats_type      pf_l2stats  
+//aggregated DC stats output from pfengine
+  ,output logic [`PF_STATBITS-1:0]  pf_dcstats_nhitmissd
+  ,output logic [`PF_STATBITS-1:0]  pf_dcstats_nhitmissp
+  ,output logic [`PF_STATBITS-1:0]  pf_dcstats_nhithit
+  ,output logic [`PF_STATBITS-1:0]  pf_dcstats_nmiss
+  ,output logic [`PF_STATBITS-1:0]  pf_dcstats_ndrop
+  ,output logic [`PF_STATBITS-1:0]  pf_dcstats_nreqs
+  ,output logic [`PF_STATBITS-1:0]  pf_dcstats_nsnoops
+  ,output logic [`PF_STATBITS-1:0]  pf_dcstats_ndisp
 
-  ,input  PF_cache_stats_type      pf0_dcstats 
-  ,input  PF_cache_stats_type      pf0_l2stats 
+//aggregated L2 stats output from pfengine
+  ,output logic [`PF_STATBITS-1:0]  pf_l2stats_nhitmissd
+  ,output logic [`PF_STATBITS-1:0]  pf_l2stats_nhitmissp
+  ,output logic [`PF_STATBITS-1:0]  pf_l2stats_nhithit
+  ,output logic [`PF_STATBITS-1:0]  pf_l2stats_nmiss
+  ,output logic [`PF_STATBITS-1:0]  pf_l2stats_ndrop
+  ,output logic [`PF_STATBITS-1:0]  pf_l2stats_nreqs
+  ,output logic [`PF_STATBITS-1:0]  pf_l2stats_nsnoops
+  ,output logic [`PF_STATBITS-1:0]  pf_l2stats_ndisp  
 
-  ,input  PF_cache_stats_type      pf1_dcstats 
-  ,input  PF_cache_stats_type      pf1_l2stats 
+//pf0_dcstats from DC to pfengine
+  ,input logic [`PF_STATBITS-1:0]  pf0_dcstats_nhitmissd
+  ,input logic [`PF_STATBITS-1:0]  pf0_dcstats_nhitmissp
+  ,input logic [`PF_STATBITS-1:0]  pf0_dcstats_nhithit
+  ,input logic [`PF_STATBITS-1:0]  pf0_dcstats_nmiss
+  ,input logic [`PF_STATBITS-1:0]  pf0_dcstats_ndrop
+  ,input logic [`PF_STATBITS-1:0]  pf0_dcstats_nreqs
+  ,input logic [`PF_STATBITS-1:0]  pf0_dcstats_nsnoops
+  ,input logic [`PF_STATBITS-1:0]  pf0_dcstats_ndisp
+
+//pf0_l2stats from L2 to pfengine
+  ,input logic [`PF_STATBITS-1:0]  pf0_l2stats_nhitmissd
+  ,input logic [`PF_STATBITS-1:0]  pf0_l2stats_nhitmissp
+  ,input logic [`PF_STATBITS-1:0]  pf0_l2stats_nhithit
+  ,input logic [`PF_STATBITS-1:0]  pf0_l2stats_nmiss
+  ,input logic [`PF_STATBITS-1:0]  pf0_l2stats_ndrop
+  ,input logic [`PF_STATBITS-1:0]  pf0_l2stats_nreqs
+  ,input logic [`PF_STATBITS-1:0]  pf0_l2stats_nsnoops
+  ,input logic [`PF_STATBITS-1:0]  pf0_l2stats_ndisp 
+
+//pf1_dcstats from DC to pfengine  
+  ,input logic [`PF_STATBITS-1:0]  pf1_dcstats_nhitmissd
+  ,input logic [`PF_STATBITS-1:0]  pf1_dcstats_nhitmissp
+  ,input logic [`PF_STATBITS-1:0]  pf1_dcstats_nhithit
+  ,input logic [`PF_STATBITS-1:0]  pf1_dcstats_nmiss
+  ,input logic [`PF_STATBITS-1:0]  pf1_dcstats_ndrop
+  ,input logic [`PF_STATBITS-1:0]  pf1_dcstats_nreqs
+  ,input logic [`PF_STATBITS-1:0]  pf1_dcstats_nsnoops
+  ,input logic [`PF_STATBITS-1:0]  pf1_dcstats_ndisp
+
+//pf1_l2stats from L2 to pfengine
+  ,input logic [`PF_STATBITS-1:0]  pf1_l2stats_nhitmissd
+  ,input logic [`PF_STATBITS-1:0]  pf1_l2stats_nhitmissp
+  ,input logic [`PF_STATBITS-1:0]  pf1_l2stats_nhithit
+  ,input logic [`PF_STATBITS-1:0]  pf1_l2stats_nmiss
+  ,input logic [`PF_STATBITS-1:0]  pf1_l2stats_ndrop
+  ,input logic [`PF_STATBITS-1:0]  pf1_l2stats_nreqs
+  ,input logic [`PF_STATBITS-1:0]  pf1_l2stats_nsnoops
+  ,input logic [`PF_STATBITS-1:0]  pf1_l2stats_ndisp
 
 `ifdef SC_4PIPE
-  ,input  PF_cache_stats_type      pf2_dcstats  // No fluid, just flop state
-  ,input  PF_cache_stats_type      pf2_l2stats  // No fluid, just flop state
 
-  ,input  PF_cache_stats_type      pf3_dcstats  // No fluid, just flop state
-  ,input  PF_cache_stats_type      pf3_l2stats  // No fluid, just flop state
+//pf2_dcstats from DC to pfengine  
+  ,input logic [`PF_STATBITS-1:0]  pf2_dcstats_nhitmissd
+  ,input logic [`PF_STATBITS-1:0]  pf2_dcstats_nhitmissp
+  ,input logic [`PF_STATBITS-1:0]  pf2_dcstats_nhithit
+  ,input logic [`PF_STATBITS-1:0]  pf2_dcstats_nmiss
+  ,input logic [`PF_STATBITS-1:0]  pf2_dcstats_ndrop
+  ,input logic [`PF_STATBITS-1:0]  pf2_dcstats_nreqs
+  ,input logic [`PF_STATBITS-1:0]  pf2_dcstats_nsnoops
+  ,input logic [`PF_STATBITS-1:0]  pf2_dcstats_ndisp 
+
+//pf2_l2stats from L2 to pfengine
+  ,input logic [`PF_STATBITS-1:0]  pf2_l2stats_nhitmissd
+  ,input logic [`PF_STATBITS-1:0]  pf2_l2stats_nhitmissp
+  ,input logic [`PF_STATBITS-1:0]  pf2_l2stats_nhithit
+  ,input logic [`PF_STATBITS-1:0]  pf2_l2stats_nmiss
+  ,input logic [`PF_STATBITS-1:0]  pf2_l2stats_ndrop
+  ,input logic [`PF_STATBITS-1:0]  pf2_l2stats_nreqs
+  ,input logic [`PF_STATBITS-1:0]  pf2_l2stats_nsnoops
+  ,input logic [`PF_STATBITS-1:0]  pf2_l2stats_ndisp
+
+//pf3_dcstats from DC to pfengine  
+  ,input logic [`PF_STATBITS-1:0]  pf3_dcstats_nhitmissd
+  ,input logic [`PF_STATBITS-1:0]  pf3_dcstats_nhitmissp
+  ,input logic [`PF_STATBITS-1:0]  pf3_dcstats_nhithit
+  ,input logic [`PF_STATBITS-1:0]  pf3_dcstats_nmiss
+  ,input logic [`PF_STATBITS-1:0]  pf3_dcstats_ndrop
+  ,input logic [`PF_STATBITS-1:0]  pf3_dcstats_nreqs
+  ,input logic [`PF_STATBITS-1:0]  pf3_dcstats_nsnoops
+  ,input logic [`PF_STATBITS-1:0]  pf3_dcstats_ndisp
+
+//pf3_l2stats from L2 to pfengine
+  ,input logic [`PF_STATBITS-1:0]  pf3_l2stats_nhitmissd
+  ,input logic [`PF_STATBITS-1:0]  pf3_l2stats_nhitmissp
+  ,input logic [`PF_STATBITS-1:0]  pf3_l2stats_nhithit
+  ,input logic [`PF_STATBITS-1:0]  pf3_l2stats_nmiss
+  ,input logic [`PF_STATBITS-1:0]  pf3_l2stats_ndrop
+  ,input logic [`PF_STATBITS-1:0]  pf3_l2stats_nreqs
+  ,input logic [`PF_STATBITS-1:0]  pf3_l2stats_nsnoops
+  ,input logic [`PF_STATBITS-1:0]  pf3_l2stats_ndisp
+
 `endif
 
  );
@@ -105,21 +192,97 @@ module pfengine_wp (
 
 `endif
 
-  ,.pf_dcstats              (pf_dcstats)
-  ,.pf_l2stats              (pf_l2stats)
+  ,.pf_dcstats              ({pf_dcstats_nhitmissd
+                            ,pf_dcstats_nhitmissp
+                            ,pf_dcstats_nhithit
+                            ,pf_dcstats_nmiss
+                            ,pf_dcstats_ndrop
+                            ,pf_dcstats_nreqs
+                            ,pf_dcstats_nsnoops
+                            ,pf_dcstats_ndisp})
 
-  ,.pf0_dcstats             (pf0_dcstats)
-  ,.pf0_l2stats             (pf0_l2stats)
+  ,.pf_l2stats              ({pf_l2stats_nhitmissd
+                            ,pf_l2stats_nhitmissp
+                            ,pf_l2stats_nhithit
+                            ,pf_l2stats_nmiss
+                            ,pf_l2stats_ndrop
+                            ,pf_l2stats_nreqs
+                            ,pf_l2stats_nsnoops
+                            ,pf_l2stats_ndisp})
 
-  ,.pf1_dcstats             (pf1_dcstats)
-  ,.pf1_l2stats             (pf1_l2stats)
+  ,.pf0_dcstats             ({pf0_dcstats_nhitmissd
+                            ,pf0_dcstats_nhitmissp
+                            ,pf0_dcstats_nhithit
+                            ,pf0_dcstats_nmiss
+                            ,pf0_dcstats_ndrop
+                            ,pf0_dcstats_nreqs
+                            ,pf0_dcstats_nsnoops
+                            ,pf0_dcstats_ndisp})
+
+  ,.pf0_l2stats             ({pf0_l2stats_nhitmissd
+                            ,pf0_l2stats_nhitmissp
+                            ,pf0_l2stats_nhithit
+                            ,pf0_l2stats_nmiss
+                            ,pf0_l2stats_ndrop
+                            ,pf0_l2stats_nreqs
+                            ,pf0_l2stats_nsnoops
+                            ,pf0_l2stats_ndisp})
+
+  ,.pf1_dcstats             ({pf1_dcstats_nhitmissd
+                            ,pf1_dcstats_nhitmissp
+                            ,pf1_dcstats_nhithit
+                            ,pf1_dcstats_nmiss
+                            ,pf1_dcstats_ndrop
+                            ,pf1_dcstats_nreqs
+                            ,pf1_dcstats_nsnoops
+                            ,pf1_dcstats_ndisp})
+
+  ,.pf1_l2stats             ({pf1_l2stats_nhitmissd
+                            ,pf1_l2stats_nhitmissp
+                            ,pf1_l2stats_nhithit
+                            ,pf1_l2stats_nmiss
+                            ,pf1_l2stats_ndrop
+                            ,pf1_l2stats_nreqs
+                            ,pf1_l2stats_nsnoops
+                            ,pf1_l2stats_ndisp})
 
 `ifdef SC_4PIPE
-  ,.pf2_dcstats             (pf2_dcstats)
-  ,.pf2_l2stats             (pf2_l2stats)
+  ,.pf2_dcstats             ({pf2_dcstats_nhitmissd
+                            ,pf2_dcstats_nhitmissp
+                            ,pf2_dcstats_nhithit
+                            ,pf2_dcstats_nmiss
+                            ,pf2_dcstats_ndrop
+                            ,pf2_dcstats_nreqs
+                            ,pf2_dcstats_nsnoops
+                            ,pf2_dcstats_ndisp})
 
-  ,.pf3_dcstats             (pf3_dcstats)
-  ,.pf3_l2stats             (pf3_l2stats)
+  ,.pf2_l2stats             ({pf2_l2stats_nhitmissd
+                            ,pf2_l2stats_nhitmissp
+                            ,pf2_l2stats_nhithit
+                            ,pf2_l2stats_nmiss
+                            ,pf2_l2stats_ndrop
+                            ,pf2_l2stats_nreqs
+                            ,pf2_l2stats_nsnoops
+                            ,pf2_l2stats_ndisp})
+
+  ,.pf3_dcstats             ({pf3_dcstats_nhitmissd
+                            ,pf3_dcstats_nhitmissp
+                            ,pf3_dcstats_nhithit
+                            ,pf3_dcstats_nmiss
+                            ,pf3_dcstats_ndrop
+                            ,pf3_dcstats_nreqs
+                            ,pf3_dcstats_nsnoops
+                            ,pf3_dcstats_ndisp})
+
+  ,.pf3_l2stats             ({pf3_l2stats_nhitmissd
+                            ,pf3_l2stats_nhitmissp
+                            ,pf3_l2stats_nhithit
+                            ,pf3_l2stats_nmiss
+                            ,pf3_l2stats_ndrop
+                            ,pf3_l2stats_nreqs
+                            ,pf3_l2stats_nsnoops
+                            ,pf3_l2stats_ndisp})
+
 `endif
 
   );
