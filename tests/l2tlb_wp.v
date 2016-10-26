c206ba720ce2a37d9c961685450c8ffc4e9ccc73
Michael Timothy Grimes <mtgrimes@unix3.lt.ucsc.edu>
2016-10-26 15:48:06 -0700

    l2tlb wrapper

--- tests/l2tlb_wp.v
+++ tests/l2tlb_wp.v
@@ -7,7 +7,7 @@ module l2tlb_wp(
   ,input                            l1tol2tlb_req_valid
   ,output                           l1tol2tlb_req_retry
   ,input  I_l1tol2tlb_req_type      l1tol2tlb_req
-  ,input  L1_reqid_type			    l1tol2tlb_req_l1id;
+  ,input  L1_reqid_type		    l1tol2tlb_req_l1id;
   ,input  logic                     l1tol2tlb_req_prefetch;
   ,input  SC_poffset_type           l1tol2tlb_req_poffset;
   ,input  TLB_hpaddr_type           l1tol2tlb_req_hpaddr;
@@ -25,32 +25,32 @@ module l2tlb_wp(
   ,output                           l2tlbtol1tlb_snoop_valid
   ,input                            l2tlbtol1tlb_snoop_retry
   ,output I_l2tlbtol1tlb_snoop_type l2tlbtol1tlb_snoop
-  ,output TLB_reqid_type    		l2tlbtol1tlb_snoop_rid;
-  ,output TLB_hpaddr_type   		l2tlbtol1tlb_snoop_hpaddr;
+  ,output TLB_reqid_type    	    l2tlbtol1tlb_snoop_rid;
+  ,output TLB_hpaddr_type   	    l2tlbtol1tlb_snoop_hpaddr;
 
   ,output                           l2tlbtol1tlb_ack_valid
   ,input                            l2tlbtol1tlb_ack_retry
   ,output I_l2tlbtol1tlb_ack_type   l2tlbtol1tlb_ack
-  ,output TLB_reqid_type    		l2tlbtol1tlb_ack_rid;
-  ,output TLB_hpaddr_type  			l2tlbtol1tlb_ack_hpaddr; // hash paddr 
-  ,output SC_ppaddr_type   			l2tlbtol1tlb_ack_ppaddr; // predicted PADDR
-  ,output SC_dctlbe_type    		l2tlbtol1tlb_ack_dctlbe; // ack
+  ,output TLB_reqid_type    	    l2tlbtol1tlb_ack_rid;
+  ,output TLB_hpaddr_type  	    l2tlbtol1tlb_ack_hpaddr; // hash paddr 
+  ,output SC_ppaddr_type   	    l2tlbtol1tlb_ack_ppaddr; // predicted PADDR
+  ,output SC_dctlbe_type    	    l2tlbtol1tlb_ack_dctlbe; // ack
 
   ,input                            l1tlbtol2tlb_req_valid
   ,output                           l1tlbtol2tlb_req_retry
   ,input  I_l1tlbtol2tlb_req_type   l1tlbtol2tlb_req
-  ,input  TLB_reqid_type		    l1tlbtol2tlb_req_rid;
-  ,input  logic             		l1tlbtol2tlb_req_disp_req; // True of disp from dcTLB (A/D bits)
-  ,input  logic            			l1tlbtol2tlb_req_disp_A;
-  ,input  logic             		l1tlbtol2tlb_req_disp_D;
-  ,input  TLB_hpaddr_type   		l1tlbtol2tlb_req_disp_hpaddr; // hash paddr 
-  ,input  SC_laddr_type    			l1tlbtol2tlb_req_laddr; // Not during disp, just req
-  ,input  SC_sptbr_type   		 	l1tlbtol2tlb_req_sptbr; // Not during disp, just req
+  ,input  TLB_reqid_type	    l1tlbtol2tlb_req_rid;
+  ,input  logic             	    l1tlbtol2tlb_req_disp_req; // True of disp from dcTLB (A/D bits)
+  ,input  logic            	    l1tlbtol2tlb_req_disp_A;
+  ,input  logic             	    l1tlbtol2tlb_req_disp_D;
+  ,input  TLB_hpaddr_type   	    l1tlbtol2tlb_req_disp_hpaddr; // hash paddr 
+  ,input  SC_laddr_type    	    l1tlbtol2tlb_req_laddr; // Not during disp, just req
+  ,input  SC_sptbr_type   	    l1tlbtol2tlb_req_sptbr; // Not during disp, just req
 
   ,input                            l1tlbtol2tlb_sack_valid
   ,output                           l1tlbtol2tlb_sack_retry
   ,input  I_l1tlbtol2tlb_sack_type  l1tlbtol2tlb_sack
-  ,input  TLB_reqid_type   			l1tlbtol2tlb_sack_rid;
+  ,input  TLB_reqid_type   	    l1tlbtol2tlb_sack_rid;
   
   //---------------------------
   // Directory interface (l2 has to arbitrate between L2 and L2TLB
@@ -58,42 +58,42 @@ module l2tlb_wp(
   ,output                           l2todr_req_valid
   ,input                            l2todr_req_retry
   ,output I_l2todr_req_type         l2todr_req
-  ,output SC_nodeid_type    		l2todr_req_nid; 
-  ,output L2_reqid_type     		l2todr_req_l2id;
-  ,output SC_cmd_type       		l2todr_req_cmd;
-  ,output SC_paddr_type     		l2todr_req_paddr;
+  ,output SC_nodeid_type  	    l2todr_req_nid; 
+  ,output L2_reqid_type             l2todr_req_l2id;
+  ,output SC_cmd_type       	    l2todr_req_cmd;
+  ,output SC_paddr_type     	    l2todr_req_paddr;
 
   ,input                            drtol2_snack_valid
   ,output                           drtol2_snack_retry
   ,input  I_drtol2_snack_type       drtol2_snack
-  ,input  SC_nodeid_type 		    drtol2_snack_nid; 
-  ,input  L2_reqid_type     		drtol2_snack_l2id; // !=0 ACK
-  ,input  DR_reqid_type     		drtol2_snack_drid; // !=0 snoop
-  ,input  SC_snack_type     		drtol2_snack_snack;
-  ,input  SC_line_type      		drtol2_snack_line;
-  ,input  SC_paddr_type     		drtol2_snack_paddr; // Not used for ACKs
+  ,input  SC_nodeid_type 	    drtol2_snack_nid; 
+  ,input  L2_reqid_type     	    drtol2_snack_l2id; // !=0 ACK
+  ,input  DR_reqid_type     	    drtol2_snack_drid; // !=0 snoop
+  ,input  SC_snack_type     	    drtol2_snack_snack;
+  ,input  SC_line_type      	    drtol2_snack_line;
+  ,input  SC_paddr_type     	    drtol2_snack_paddr; // Not used for ACKs
 
   ,output                           l2todr_snoop_ack_valid
   ,input                            l2todr_snoop_ack_retry
   ,output I_l2snoop_ack_type        l2todr_snoop_ack
-  ,output L2_reqid_type     		l2todr_snoop_ack_l2id;
+  ,output L2_reqid_type     	    l2todr_snoop_ack_l2id;
 
   ,output                           l2todr_disp_valid
   ,input                            l2todr_disp_retry
   ,output I_l2todr_disp_type        l2todr_disp
-  ,output SC_nodeid_type   			l2todr_disp_nid; 
-  ,output L2_reqid_type   			l2todr_disp_l2id; // != means L2 initiated disp (drid==0)
-  ,output DR_reqid_type    			l2todr_disp_drid; // !=0 snoop ack. (E.g: SMCD_WI resulting in a disp)
-  ,output SC_disp_mask_type 		l2todr_disp_mask;
-  ,output SC_dcmd_type      		l2todr_disp_dcmd;
-  ,output SC_line_type      		l2todr_disp_line;
-  ,output SC_paddr_type     		l2todr_disp_paddr;
+  ,output SC_nodeid_type   	    l2todr_disp_nid; 
+  ,output L2_reqid_type   	    l2todr_disp_l2id; // != means L2 initiated disp (drid==0)
+  ,output DR_reqid_type    	    l2todr_disp_drid; // !=0 snoop ack. (E.g: SMCD_WI resulting in a disp)
+  ,output SC_disp_mask_type 	    l2todr_disp_mask;
+  ,output SC_dcmd_type      	    l2todr_disp_dcmd;
+  ,output SC_line_type      	    l2todr_disp_line;
+  ,output SC_paddr_type     	    l2todr_disp_paddr;
 
   ,input                            drtol2_dack_valid
   ,output                           drtol2_dack_retry
   ,input  I_drtol2_dack_type        drtol2_dack
-  ,input  SC_nodeid_type 		    drtol2_dack_nid; 
-  ,input  L2_reqid_type     		drtol2_dack_l2id;
+  ,input  SC_nodeid_type 	    drtol2_dack_nid; 
+  ,input  L2_reqid_type     	    drtol2_dack_l2id;
   /* verilator lint_on UNUSED */
 );
 
