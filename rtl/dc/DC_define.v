11ec1a12a2c966f06bd59552ce1b3a97d3c609f5
Nursultan Kabylkas <kabylkas@gmail.com>
2016-11-21 11:28:19 -0800

    worked on 64/32/16/8 bit loads

---
+++ rtl/dc/DC_define.v
@@ -0,0 +1,19 @@
+
+`define	 SETS	  	  32
+`define  WAYS	  	  8
+`define  TAGBITS  	  18
+`define  SET_INDEX_BITS   5
+`define  WAY_BITS 	  3
+`define  CACHE_STATE  	  3
+
+//Cache States 3 bits
+
+`define  M  	3'b000
+`define  E  	3'b001
+`define  S  	3'b010
+`define  I  	3'b011
+`define  US  	3'b100
+`define  UM  	3'b101
+`define  LOCK  	3'b110
+`define  MARK  	3'b111
+
